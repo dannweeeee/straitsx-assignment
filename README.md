a011835cbd6a063669d3cb2a7e177533812f1767
Dann Wee <dannweezj@gmail.com>
2024-03-27 00:59:07 +0800

    update readme and created .env.example and queryAccountBalance files

--- README.md
+++ README.md
@@ -1,5 +1,52 @@
 # StraitsX Take Home Test
 
+## Contract Address
+
 ```
-Deployed Contract Address: 0x65a924ab338b38Aea527FEa79Da21B2cf706e3b3
+Deployed Contract Address: 0x1AFdaD6Ac74b117433aaA8C88EF95669839b7B0d
+```
+
+## Setup
+
+1. Fork the `.env.example` file and rename it to `.env`. Fill in the required fields.
+
+2. Install Dependencies
+
+```
+npm install
+```
+
+3. Compile Contracts
+
+```
+npx hardhat compile
+```
+
+## Commands to test smart contract
+
+### Transfer 1000 tokens to a second account
+
+```
+npx hardhat run --network sepolia ./ignition/scripts/transferTokens.js
+```
+
+### Deposit 500 tokens to the contract
+
+```
+npx hardhat run --network sepolia ./ignition/scripts/depositTokens.js
+```
+
+### Withdraw 500 tokens from the contract
+
+For this function, I removed the ability to input a specific amount of tokens to withdraw to avoid owners from withdrawing more funds than they have deposited.
+The amount to withdraw is based on a deposits mapping that keeps track of the amount of tokens deposited by each address.
+
+```
+npx hardhat run --network sepolia ./ignition/scripts/withdrawTokens.js
+```
+
+### Query account balance
+
+```
+npx hardhat run --network sepolia ./ignition/scripts/queryAccountBalance.js
 ```
--- ignition/scripts/queryAccountBalance.js
+++ ignition/scripts/queryAccountBalance.js
@@ -1,37 +1,23 @@
 require("dotenv").config();
 const { ethers } = require("hardhat");
 
-async function withdrawTokens() {
+async function queryAccountBalance() {
     const provider = new ethers.JsonRpcProvider(`https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`);
 
     const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
     const contractAddress = "0x1AFdaD6Ac74b117433aaA8C88EF95669839b7B0d";
-    const fromAddress = "0x1dF6bE628F34a964AcFb695CD9F5417DBc95ff42";
+    const ownerAddress = "0x1dF6bE628F34a964AcFb695CD9F5417DBc95ff42";
 
     const Token = await ethers.getContractFactory("Token");
     const token = Token.attach(contractAddress);
 
-    // Get the balance of the fromAddress
-    const fromBalance = await token.balanceOf(fromAddress);
-    console.log(`Balance of fromAddress: ${ethers.formatUnits(fromBalance, 18)}`);
-
-    const amount = 500;
-    const tx = await token.connect(signer).withdraw(amount);
-    await tx.wait();
-
-    // Get the updated balance after the transfer
-    const updatedFromBalance = await token.balanceOf(fromAddress);
-    console.log(`Updated balance of fromAddress: ${ethers.formatUnits(updatedFromBalance, 18)}`);
-
-    // Get the total supply after the withdrawal
-    const totalSupply = await token.totalSupply();
-    console.log(`Total supply after withdrawal: ${ethers.formatUnits(totalSupply, 18)}`);
-
-    console.log("Tokens withdrawn successfully");
+    // Get the balance of the ownerAddress
+    const fromBalance = await token.balanceOf(ownerAddress);
+    console.log(`Balance of ownerAddress: ${ethers.formatUnits(fromBalance, 18)}`);
 }
 
-withdrawTokens()
+queryAccountBalance()
     .then(() => process.exit(0))
     .catch((error) => {
         console.error(error);
